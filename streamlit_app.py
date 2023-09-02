586b29c7e6937a323e3c10d06e878f7fbe0b36f7
snowflakesunny <143302323+snowflakesunny@users.noreply.github.com>
2023-09-01 20:28:34 -0500

    Update streamlit_app.py
    More code added.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title("My Parents New healthy Dinner.")
 streamlit.header("Breakfast Menu")
+streamlit.text("Sooji Upma with vegetables")
+streamlit.text("Idli Vada and Sambar")
+streamlit.text("Lenthil Payasam")
