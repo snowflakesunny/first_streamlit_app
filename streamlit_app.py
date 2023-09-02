b90e07f37c2c83e0b443130f9826abb202e2e1d5
snowflakesunny <143302323+snowflakesunny@users.noreply.github.com>
2023-09-01 20:48:52 -0500

    Update streamlit_app.py
    adding fruit list to show

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
 import pandas
-
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.title("🥣My Parents New healthy Dinner.")
 streamlit.header("Breakfast Menu")
 streamlit.text("🥗Sooji Upma with vegetables")
 streamlit.text("🐔 Idli Vada and Sambar")
 streamlit.text("🥑Lenthil Payasam")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
