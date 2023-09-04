097ef9af6f9b8409c03d00f34a505d09c3024007
snowflakesunny <143302323+snowflakesunny@users.noreply.github.com>
2023-09-04 15:53:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,17 +35,8 @@ try:
 	fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 	streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
-	streamlit.error()
-_ = """
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+streamlit.error()
 
-#streamlit.text(fruityvice_response.json())
-# Displaying the furitvise json response using Pandas
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Showing in a dataframe to beautify it
-streamlit.dataframe(fruityvice_normalized)
-"""
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
