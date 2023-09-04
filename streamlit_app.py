7a2339a215bf3aac1f94198238690e9c3683e379
snowflakesunny <143302323+snowflakesunny@users.noreply.github.com>
2023-09-04 16:39:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
-#Requests from fruityvice
+#Create the repeatable code block (Called a function)
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
+#Requests from fruityvice response from an API
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 try:
@@ -30,9 +36,11 @@ try:
   if not fruit_choice:
        streamlit.error("Please select a fruit to get information.")
   else:
-       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-       streamlit.dataframe(fruityvice_normalized)
+       back_from_function = get_fruityvice_data(fruit_choice)
+       streamlit.dataframe(back_from_function)
+       #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+       #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+       #streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
   streamlit.error()
