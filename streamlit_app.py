d83b0194e94772470555eb648a8087045a80ddb7
snowflakesunny <143302323+snowflakesunny@users.noreply.github.com>
2023-09-04 15:22:23 -0500

    Update streamlit_app.py
    error fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,9 +33,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Showing in a dataframe to beautify it
 streamlit.dataframe(fruityvice_normalized)
-
 streamlit.stop()
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
